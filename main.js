0515c814edd31ea1695b6d6ae471930ba5b91ee6
ornella <monticello.ornella@gmail.com>
2025-06-13 11:07:46 -0300

    ultimos cambios segun las correcciones

--- main.js
+++ main.js
@@ -3,29 +3,29 @@ const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
+  nav.classList.add("visible");
 });
 
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
+  nav.classList.remove("visible");
 });
 
 function mostrarGaleria(id, bloquearScroll = false) {
-    document.getElementById(id).style.display = 'flex';
-    if (bloquearScroll) {
-      document.body.classList.add('bloquear-scroll');
-    }
+  document.getElementById(id).style.display = 'flex';
+  if (bloquearScroll) {
+    document.body.classList.add('bloquear-scroll');
   }
-  
-  function ocultarGaleria(id, bloquearScroll = false) {
-    document.getElementById(id).style.display = 'none';
-    if (bloquearScroll) {
-      document.body.classList.remove('bloquear-scroll');
-    }
+}
+
+function ocultarGaleria(id, bloquearScroll = false) {
+  document.getElementById(id).style.display = 'none';
+  if (bloquearScroll) {
+    document.body.classList.remove('bloquear-scroll');
   }
+}
 
 
-  window.addEventListener("click", function () {
-    const audio = document.getElementById("musica");
-    audio.play();
-  }, { once: true }); 
+window.addEventListener("click", function () {
+  const audio = document.getElementById("musica");
+  audio.play();
+}, { once: true }); 
